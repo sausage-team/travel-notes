8169ad68976062e0131c9a2678563bc6ef04967c
tangdaoyuan <1197633750@qq.com>
2019-05-14 22:40:22 +0800

    update sql

--- dockerfiles/mysql/sql-scripts/CreateTable.sql
+++ dockerfiles/mysql/sql-scripts/CreateTable.sql
@@ -23,7 +23,7 @@ CREATE TABLE `travel_article` (
   PRIMARY KEY (`id`),
   KEY `travel_article_user_id_188d6f31_fk_travel_user_id` (`user_id`),
   CONSTRAINT `travel_article_user_id_188d6f31_fk_travel_user_id` FOREIGN KEY (`user_id`) REFERENCES `travel_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `travel_articleimage` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,4 +33,4 @@ CREATE TABLE `travel_articleimage` (
   `created_time` datetime(6) NOT NULL,
   `updated_time` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
